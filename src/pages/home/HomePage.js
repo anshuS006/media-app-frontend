1c34a9b64f42d1769dc6eed17a536c0db3503118
Anshuman Pratap Singh <anshuman.pratap@jagrannewmedia.com>
2025-03-18 21:53:02 +0530

    test

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="max-w-lg p-6 bg-white shadow-lg rounded-lg text-center">
-        <h1 className="text-2xl font-bold text-gray-800">Welcome Staging</h1>
+        <h1 className="text-2xl font-bold text-gray-800">Welcome Staging anshuman</h1>
         <p className="text-gray-600 mt-2">This is a simple static webpage built with React and Tailwind CSS.</p>
       </div>
     </div>
